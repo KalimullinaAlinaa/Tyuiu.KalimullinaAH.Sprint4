4f34fd276b3d79133b79cf3e745f36b511bf8f79
Tyuiu.KalimullinaAH@outlook.com <boulevarovaalina@icloud.com>
2023-11-10 10:46:53 +0500

    Task4

---
+++ Tyuiu.KalimullinaAH.Sprint4.Task4.V16.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KalimullinaAH.Sprint4.Task4.V16.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KalimullinaAH.Sprint4.Task4.V16.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9586c979-cd89-435e-86a6-334849219a7d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.KalimullinaAH.Sprint4.Task4.V16/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KalimullinaAH.Sprint4.Task4.V16.Lib;
+namespace Tyuiu.KalimullinaAH.Sprint4.Task4.V16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #4 | Выполнила: Калимуллина А.Х | ИИПб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Одномерные массивы (статический ввод)                             *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #16                                                              *");
+            Console.WriteLine("* Выполнила: Калимуллина Алина Халимовна | ИИПб-23-1                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный        *");
+            Console.WriteLine("* значениями с клавиатуры в диапазоне от 3 до 7. Заменить четные элементы *");
+            Console.WriteLine("* массива на 1.                                                           *");
+            Console.WriteLine("* 6, 5, 6, 7, 3,                                                           *");
+            Console.WriteLine("* 5, 4, 6, 7, 3,                                                          *");
+            Console.WriteLine("* 5, 5, 5, 6, 5,                                                          *");
+            Console.WriteLine("* 6, 5, 4, 4, 3,                                                          *");
+            Console.WriteLine("* 4, 5, 4, 5, 5,                                                          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.Write("Введите количество строк в массиве:");
+            int rows = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите количество столбцов в массиве:");
+            int columns = Convert.ToInt32(Console.ReadLine());
+            int[,] mtrx = new int[rows, columns];
+
+            Console.WriteLine("***************************************************************************");
+            for (int i = 0; i < mtrx.GetLength(0); i++)
+            {
+                for (int j = 0; j < mtrx.GetLength(1); j++)
+                {
+                    Console.Write($"Введите {i },{j} элемент массива: ");
+                    mtrx[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("Массив:");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{mtrx[i, j]} \t");
+                }
+                Console.WriteLine();
+
+            }
+            int[,] res = ds.Calculate(mtrx);
+
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ: *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine();
+
+
+            for (int i = 0; i < res.GetLength(0); i++)
+            {
+                for (int j = 0; j < res.GetLength(1); j++)
+                {
+                    Console.Write(res[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
