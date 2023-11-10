07a569b1d9f7dc6c1e8cbdb8dd7517b92e4d9316
Tyuiu.KalimullinaAH@outlook.com <boulevarovaalina@icloud.com>
2023-11-10 12:46:05 +0500

    Task7

---
+++ Tyuiu.KalimullinaAH.Sprint4.Task7.V18.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.KalimullinaAH.Sprint4.Task7.V18.Lib;
+namespace Tyuiu.KalimullinaAH.Sprint4.Task7.V18.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void VaidCalc()
+        {
+            DataService ds = new DataService();
+            int rows = 5;
+            int columns = 3;
+            int[,] mtrx = new int[rows, columns];
+            string str = "145258749635789";
+            int res = ds.Calculate(rows, columns, str);
+            int wait = 12288;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.KalimullinaAH.Sprint4.Task7.V18/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KalimullinaAH.Sprint4.Task7.V18.Lib;
+namespace Tyuiu.KalimullinaAH.Sprint4.Task7.V18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Random rnd = new Random();
+            Console.Title = "Спринт #4 | Выполнила: Калимуллина А.Х | ИИПб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #18                                                             *");
+            Console.WriteLine("* Выполнила: Калимуллина Алина Халимовна | ИИПб-23-1                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дана строка из одноразрядных цифр \"145258749635789\".                  *");
+            Console.WriteLine("*  Преобразуйте ее в матрицу4 на 2 и подсчитайте количество               *");
+            Console.WriteLine("* нечетных чисел в матрице.                                               *");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int rows = 5;
+            int columns = 3;
+            int[,] matrix = new int[rows, columns];
+
+            string str = "145258749635789";
+
+            int index = 0;
+
+            Console.WriteLine("\nМассив:");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{str[index]} \t");
+                    index++;
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ: *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.Write(" Произведение четных чисел = " + ds.Calculate(rows, columns, str));
+
+            Console.ReadLine();
+        }
+    }
+}
